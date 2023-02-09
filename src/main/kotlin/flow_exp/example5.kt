1e60ae2cdb8e48107858965eaeb74aae2c5bd190
dalakoti07 <saurabhdalakoti07@gmail.com>
2023-02-09 16:32:48 +0530

    added flatmapcontact, flatmapmerge and flatmaplatest examples

---
+++ src/main/kotlin/flow_exp/example5.kt
@@ -0,0 +1,25 @@
+package flow_exp
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+
+suspend fun main(){
+
+    val stateFlow = MutableStateFlow("initial")
+
+    val scope =
+        CoroutineScope(Dispatchers.IO + Job())
+
+    scope.launch {
+        stateFlow.collect {
+            println("flow: $it")
+        }
+    }
+
+    for(i in listOf("saurabh","abhishek","anuj","aman")){
+        delay(1000)
+        stateFlow.value = i
+    }
+
+}
