525ce5ff34964481513054c30a056dc2ca3c781a
Saurabh Dalakoti <saurabhdalakoti@BMLAP317.local>
2023-07-14 15:02:52 +0530

    usage of newSingleThreadContext

---
+++ src/main/kotlin/coroutines/battleTests/singleThreadContext.kt
@@ -0,0 +1,13 @@
+package coroutines.battleTests
+
+import kotlinx.coroutines.*
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main() = runBlocking{
+    val scope = CoroutineScope(newSingleThreadContext("meraThread"))
+    for(i in 1..100){
+        scope.launch {
+            println("Current thread ${Thread.currentThread()}")
+        }
+    }
+}
