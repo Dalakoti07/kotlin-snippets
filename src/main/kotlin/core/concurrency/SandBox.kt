4213f5ccf010efc42d715a33e42424813acd378e
Saurabh Dalakoti <saurabhdalakoti@BMLAP317.local>
2024-01-10 16:03:46 +0530

    example added

--- src/main/kotlin/core/concurrency/SandBox.kt
+++ src/main/kotlin/core/concurrency/SandBox.kt
@@ -4,42 +4,62 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.util.*
+import kotlin.random.Random
 
 class PlayGround {
 
     private val items = LinkedList<Int>()
+    private val mutex = Mutex()
 
     fun run(){
         val coroutinesScope1 = CoroutineScope(Dispatchers.Default)
         val coroutinesScope2 = CoroutineScope(Dispatchers.Default)
-        items.addAll(listOf(1,2,3,5,6))
+        val coroutinesScope3 = CoroutineScope(Dispatchers.Default)
+        items.addAll(listOf(1,2,3,5,6,7,8,9))
 
-        coroutinesScope1.launch {
-            removeAll()
+        coroutinesScope3.launch {
+            while (true){
+                delay(500)
+                println("added 4 elems")
+                items.addAll(listOf(
+                    Random.nextInt(),
+                    Random.nextInt(),
+                    Random.nextInt(),
+                    Random.nextInt(),
+                ))
+            }
         }
+
         coroutinesScope2.launch {
-            removeItem()
+            while (true){
+                delay(1000)
+                removeItem()
+            }
+        }
+        coroutinesScope1.launch {
+            while (true){
+                delay(500)
+                removeAll()
+            }
         }
     }
 
     private suspend fun removeItem() {
         if(items.isEmpty()) {
+            println("is empty")
             return
         }
-        delay(1000)
-        val last = items.last()
-        items.remove()
-        println("removeItem ....")
-        delay(1000)
-        removeItem()
+        delay(500)
+        val last = items.last
+        println("$last removed ....")
     }
 
-    private suspend fun removeAll() {
-        delay(500)
+    private fun removeAll() {
         items.clear()
         println("removeAll ....")
-        removeAll()
     }
 
 
